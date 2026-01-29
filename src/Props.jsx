6b0db8745fef2623295e30537c5c54022c64aebd
Mustajab <r2whiphop@gmail.com>
2026-01-29 20:26:10 +0500

    Props In React

---
+++ src/Props.jsx
@@ -0,0 +1,28 @@
+
+function Propss(props){
+      
+    return (
+        <>
+{/* 
+        <h2>Users Data From Object</h2>
+        <h3>{props.object.name}</h3>
+        <h3>{props.object.email}</h3>
+        <h3>{props.object.code}</h3>
+
+
+        
+        <h2>Users Data From Array</h2>
+        <h3>{props.array[0]}</h3>
+        <h3>{props.array[1]}</h3>
+        <h3>{props.array[2]}</h3>
+
+        <h2>This is String {props.string}</h2>
+        <h2>This is Number {props.number}</h2> */}
+
+
+
+    </>
+    )
+}
+
+export default Propss
