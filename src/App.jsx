e08d9f7c58300db8b9d9922ddd8037a0a4ba29f1
Mustajab <r2whiphop@gmail.com>
2026-01-31 01:52:17 +0500

    Props Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
-import Header from './Header'
+// import Header from './Header'
+// import Conditonals from './Conditionals'
+// import Attributes from './Attributes'
+import Propss from './Props'
 
 function App() {
 
+
+  // let obj ={name: "mustajab obj", code:328742, email:"mustajab@object.com"}
+  // let arr = ["mustajab arr", 183313, "mustajab@array.com"]
+  // let str = "falcon"
+  // let num = 273872
+
+
+
+
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      {/* <Propss  object={obj} array={arr} string={str} number={num}/> */}
+      {/* <Conditonals /> */}
+      {/* <Attributes /> */}
+
     </>
   )
 }
