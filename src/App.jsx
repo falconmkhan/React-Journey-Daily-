185b9aab10ce89635ad1b3efdd6e7b5b06f0753b
Mustajab <r2whiphop@gmail.com>
2026-02-01 23:17:42 +0500

    Conditionals In React JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import './App.css'
 // import Header from './Header'
-// import Conditonals from './Conditionals'
+import Conditonals from './Conditionals'
 // import Attributes from './Attributes'
-import Propss from './Props'
+// import Propss from './Props'
 
 function App() {
 
 
+  // Variables For Propss Component===========
   // let obj ={name: "mustajab obj", code:328742, email:"mustajab@object.com"}
   // let arr = ["mustajab arr", 183313, "mustajab@array.com"]
   // let str = "falcon"
   // let num = 273872
 
 
+  // Variables For Condionals Component===========
+  // let action = "working"
+  // let limit = 9
+  // let model = "Lenove Thinkpad T14"
 
 
   return (
     <>
       {/* <Header /> */}
       {/* <Propss  object={obj} array={arr} string={str} number={num}/> */}
-      {/* <Conditonals /> */}
+      {/* <Conditonals action={action} limit={limit} model={model}/>  */}
       {/* <Attributes /> */}
 
     </>
