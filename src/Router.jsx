de70a05ee79172f3f459528189934d71e87a90df
Mustajab <r2whiphop@gmail.com>
2026-02-04 22:28:36 +0500

    Routers In React, Will Upload Soon

---
+++ src/Router.jsx
@@ -0,0 +1,13 @@
+
+
+const Events = () => {
+    return (
+        <>
+            <h1> Router in React</h1>
+            <h3> What is React Router? 
+                React Router is a library that provides routing capabilities for React applications.
+            </h3>
+        </>
+    )
+}
+export default Events
